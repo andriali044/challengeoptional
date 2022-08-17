0caf4fa88569fd7f97a5b62f3e8f5d6887066e35
andriali044 <andriali044gmail.com>
2022-08-18 00:20:49 +0700

    optional

---
+++ script.js
@@ -0,0 +1,32 @@
+alert('assalamualaikum')
+var rahasia = parseInt(prompt('masukkan angka :'));
+if( rahasia === 1 ){
+    alert('Alhamdulillah');
+} else if (rahasia === 8 ) {
+    alert('Alhamdulillah');
+} else if (rahasia === 3 ) {
+    alert('subhanallah');
+} else if (rahasia === 5 ) {
+    alert('subhanallah');
+} else if (rahasia === 9 ) {
+    alert('subhanallah');
+} else if (rahasia === 1 ) {
+    alert('subhanallah');
+}
+
+var tebakan = parseInt(prompt('masukkan angka :'));
+if( tebakan === 1 ){
+    alert('subhanallah')
+} else if (tebakan === 8 ) {
+    alert('Alhamdulillah');
+} else if (tebakan === 3 ) {
+    alert('subhanallah');
+} else if (tebakan === 5 ) {
+    alert('subhanallah');
+} else if (tebakan === 9 ) {
+    alert('subhanallah');
+} else if (tebakan === 1 ) {
+    alert('subhanallah');
+}
+
+alert('Terima Kasih ...')
